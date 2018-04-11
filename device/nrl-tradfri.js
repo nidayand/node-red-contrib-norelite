015ee8335562008c4632636d2b955e019a374ddb
Peter Göthager <petergothager@Peters-MacBook-Pro.local>
2018-04-11 15:38:45 +0200

    Fix

--- device/nrl-tradfri.js
+++ device/nrl-tradfri.js
@@ -21,15 +21,25 @@ module.exports = function (RED) {
 
       var brightness = 255;
       if (self.dimmable) {
-        brightness = Math.round(nmsg.getDim() / 100 * 255);
+        brightness = Math.round(omsg.getDim() / 100 * 255);
       }
 
       nmsg.payload = {
         brightness: brightness,
-        state: ((omsg.is_enabled() || brightness == 0) ? 'on' : 'off'),
+        state: (!(!omsg.is_enabled() || brightness == 0) ? 'on' : 'off'),
         color: omsg.getColor().replace('#', '').toLowerCase(),
         transistiontime: 0
       };
+
+      // Set node text
+      var state = -1;
+      if (!(!omsg.is_enabled() || brightness == 0))
+        state = 1;
+      var txt = (state == 1 ? "On" : "Off");
+      if (self.dimmable && state == 1)
+        txt = "Dim " + omsg.getDim() + "%";
+
+      common.setStatus(self, state, txt);
 
       self.send(nmsg);
     });
